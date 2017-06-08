eb190bc985bf6bfffcaa7f599dff10f99f7db3c4
perqin <perqinxie@gmail.com>
2017-06-08 13:12:20 +0800

    Add virtualenv

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-uwsgi -s /tmp/eatpotmovie-ci.sock --manage-script-name --mount /=managy:app
+uwsgi -s /tmp/eatpotmovie-ci.sock --manage-script-name --mount /=managy:app --plugin python3 --virtualenv ./venv/
 
 exit 0
