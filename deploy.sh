8fb2905b9a6f667fda1d7f61881517205ee54d4b
perqin <perqinxie@gmail.com>
2017-06-09 11:27:26 +0800

    Maybe final fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-uwsgi -s /tmp/eatpotmovie-ci.sock --manage-script-name --mount /=managy:app --plugin python3 --virtualenv ./venv/
+if [ "$YOURAPPLICATION_SETTINGS" = "" ]; then
+    echo "Env var YOURAPPLICATION_SETTINGS not set, exit..."
+    exit 1
+fi
+
+uwsgi -s /tmp/eatpotmovie-ci.sock --manage-script-name --mount /=managy:app --virtualenv ./venv/
 
 exit 0
